499fd7bd7c7cc37ee11918567daee1ceb383c6ce
msaunders-twilio <msaunders.twilio@gmail.com>
2023-04-24 16:59:13 -0400

    Add paste theme provider, use EditorToggle component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Theme} from '@twilio-paste/core/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Theme.Provider theme="evergreen" style={{ height: '100%', width: '100%' }}>
+      <App />
+    </Theme.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
